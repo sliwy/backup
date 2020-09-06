ce72272c3a047c778cbe800e6417135809ae1a08
Maciej Sliwowski <macieksliwowski@gmail.com>
2020-09-06 19:37:29 +0200

    one more soft backup script plus strict backup script to synchronize folders

--- strict-backup.py
+++ strict-backup.py
@@ -51,7 +51,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     'source_path',
     help='Source path for backup. All files and directories recursively inside '
-         'the specified directory will be backed up.')
+         'the specified directory will be synchronized.')
 parser.add_argument(
     'target_path',
     help='Path where all the files from source_path will be saved.')
@@ -66,6 +66,17 @@ if value != 'y':
     exit(0)
 
 all_source_paths = list(source_path.rglob('*'))
+# removing removed dirs
+for path in target_path.rglob('*'):
+    if replace_with_source(path) not in all_source_paths:
+        try:
+            if path.is_file():
+                path.unlink()
+        except FileNotFoundError:
+            pass
+for path in target_path.rglob('*'):
+    if path.is_dir() and replace_with_source(path) not in all_source_paths:
+        path.rmdir()
 
 for path in tqdm(all_source_paths):
     copy_target_path = replace_with_target(path)
@@ -73,12 +84,9 @@ for path in tqdm(all_source_paths):
         if not copy_target_path.exists():
             os.makedirs(str(copy_target_path.parent), exist_ok=True)
             shutil.copy(str(path), str(copy_target_path))
-        elif copy_target_path.exists() and not same_file(path, copy_target_path):
-            value = input('File {} (in source: {}) exists and was modified in source.'
-                          ' Do you wish to overwrite in target location?\ny/n'.format(
-                copy_target_path, path))
-            if value == 'y':
-                copy_target_path.unlink()
-                shutil.copy(str(path), str(copy_target_path))
+        elif copy_target_path.exists() and not same_file(path,
+                                                         copy_target_path):
+            copy_target_path.unlink()
+            shutil.copy(str(path), str(copy_target_path))
     elif path.is_dir():
         copy_target_path.mkdir(parents=True, exist_ok=True)
